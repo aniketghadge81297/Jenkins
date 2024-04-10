23cc3840f15580effc43d2833cf34e60aa285aaf
aniketghadge81297 <153165781+aniketghadge81297@users.noreply.github.com>
2024-04-10 16:07:44 +0530

    Add files via upload

--- dis.groovy
+++ dis.groovy
@@ -4,28 +4,29 @@ pipeline {
     stages {
         stage('pull') {
             steps {
+                git branch: 'main', changelog: false, credentialsId: 'ssh', poll: false, url: 'https://github.com/aniketghadge81297/Jenkins.git'
                 echo 'this is pull operation'
             }
         }
-        stage('build') {
-            steps {
-                echo 'this is build operation'
-            }
-        }
-        stage('test') {
-            steps {
-                echo 'this is test operation'
-            }
-        }
-        stage('quallity check') {
-            steps {
-                echo 'this is quality check operation'
-            }
-        }
-        stage('deploy') {
-            steps {
-                echo 'this is deploy operation'
-            }
-        }
+        // stage('build') {
+        //     steps {
+        //         echo 'this is build operation'
+        //     }
+        // }
+        // stage('test') {
+        //     steps {
+        //         echo 'this is test operation'
+        //     }
+        // }
+        // stage('quallity check') {
+        //     steps {
+        //         echo 'this is quality check operation'
+        //     }
+        // }
+        // stage('deploy') {
+        //     steps {
+        //         echo 'this is deploy operation'
+        //     }
+        // }
     }
 }
